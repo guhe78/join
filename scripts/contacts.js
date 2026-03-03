38cdf2ec163814202be53fa4fd1e4e6955358ad5
Guenter Heldt <gh1978@hotmail.de>
2026-03-03 08:39:23 +0100

    added hover effect onclick style

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,5 +1,6 @@
 const contactsList = document.getElementById("contacts-list");
 let contacts = [];
+let letterBefore = "Z";
 
 async function init() {
   await fetchContacts();
@@ -9,20 +10,31 @@ async function init() {
 async function fetchContacts() {
   const response = await fetch("../scripts/contacts.json");
   contacts = await response.json();
+
+  contacts.sort((a, b) => a.firstName.localeCompare(b.firstName, "de"));
 }
 
 function renderContactsList() {
   contactsList.innerHTML = "";
   for (let i = 0; i < contacts.length; i++) {
+    let contact = contacts[i];
+    let letter = contact.firstName[0].toUpperCase();
+    console.log(letter);
+    if (letterBefore != letter) {
+      letterBefore = letter;
+      console.log("letterBefore: " + letterBefore);
+      contactsList.innerHTML += contactLetterTemplate(letter);
+    }
     contactsList.innerHTML += contactTemplate(i);
   }
 }
 
 function contactTemplate(index) {
   const contact = contacts[index];
+
   return `
-        <div class="contact">
-        <div class="contact-badge">${contact.firstName[0].toUpperCase()}${contact.lastName[0].toUpperCase()}</div>
+        <div class="contact" onclick="toggleActiveContact(event)">
+          <div class="contact-badge">${contact.firstName[0].toUpperCase()}${contact.lastName[0].toUpperCase()}</div>
           <div class="contact-data">
             <p class="name">${contact.firstName} ${contact.lastName}</p>
             <p class="email">${contact.email}</p>
@@ -30,3 +42,25 @@ function contactTemplate(index) {
         </div>
     `;
 }
+
+function contactLetterTemplate(letter) {
+  return `
+    <div class="contact-letter">${letter}</div>
+    <div class="contact-seperator"></div>
+  `;
+}
+
+function toggleActiveContact(event) {
+  const activeElement = document.querySelector(".active-contact");
+  if (activeElement) {
+    activeElement.classList.remove("active-contact");
+  }
+
+  if (activeElement === event.currentTarget) {
+    event.currentTarget.classList.remove("active-contact");
+  } else {
+    event.currentTarget.classList.add("active-contact");
+  }
+}
+
+function addNewContact() {}
