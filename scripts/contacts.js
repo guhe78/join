7b02c962b428b7cadb38016616a67f2f1b3699b9
Guenter Heldt <gh1978@hotmail.de>
2026-04-04 13:57:03 +0200

    fixed bugs failures contacts

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -400,7 +400,6 @@ async function saveEditedContact(firebaseKey) {
   const contact = findContact(firebaseKey);
   if (!validateForm()) return;
   const contactName = splitName(DOM.contactNameEl.value);
-  console.log(contactName);
   if (!contactName) return;
   contact.firstName = contactName.firstName;
   contact.lastName = contactName.lastName;
