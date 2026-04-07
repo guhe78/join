ccd6b2ad9c8aee63183370920ed61bc623cc3d43
NilDACE <nil.vollhardt@gmail.com>
2026-04-07 09:50:57 +0200

    feat: update board scripts and add utility functions for task contact management

--- scripts/board-state.js
+++
@@ -1,6 +0,0 @@
-/** @type {string|null} Stores the ID of the element currently being dragged */
-let currentDraggedElement = null;
-
-let currentTasks = [];
-let tippTimer;
-const TaskDialogCloseDuration = 200;
