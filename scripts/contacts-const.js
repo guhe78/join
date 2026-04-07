4c765f541de75c9139d2dbd0a35f9103bbfe369f
Guenter Heldt <gh1978@hotmail.de>
2026-04-07 09:53:32 +0200

    fecactor code organize structure summary renaming files contacts

---
+++ scripts/contacts-const.js
@@ -0,0 +1,18 @@
+const MOBILE_BREAKPOINT = 850;
+const DEFAULT_BADGE_COLORS = [
+  "#ff7a00",
+  "#9327ff",
+  "#6e52ff",
+  "#fc71ff",
+  "#1fd7c1",
+  "#ff5eb3",
+  "#00bee8",
+  "#ff745e",
+  "#ffc701",
+  "#0038ff",
+  "#c3ff2b",
+  "#ffe62b",
+  "#ff4646",
+  "#ffbb2b",
+  "#ffa35e",
+];
