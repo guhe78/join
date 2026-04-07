2bbe0b7b1a66a92838c8502cec2faf30b6e0a262
NilDACE <nil.vollhardt@gmail.com>
2026-04-07 09:36:18 +0200

    feat: add task management functions and UI updates for drag-and-drop functionality

---
+++ scripts/board-state.js
@@ -0,0 +1,6 @@
+/** @type {string|null} Stores the ID of the element currently being dragged */
+let currentDraggedElement = null;
+
+let currentTasks = [];
+let tippTimer;
+const TaskDialogCloseDuration = 200;
